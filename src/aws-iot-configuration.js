517c62ee3af157294bcc87aec997f0540ae29c82
clash9529 <clash9529@gmail.com>
2023-11-16 13:06:28 +0800

    first commit

---
+++ src/aws-iot-configuration.js
@@ -0,0 +1,7 @@
+var awsIotConfiguration = {
+    endpoint: 'wss://a30kc1962iresi-ats.iot.ap-south-1.amazonaws.com/mqtt',
+    region: 'ap-south-1',
+    poolId: 'ap-south-1:ebd832fd-9e9b-4d89-871e-232b40b90d79',
+    host: 'a30kc1962iresi-ats.iot.ap-south-1.amazonaws.com'
+  };
+ module.exports = awsIotConfiguration;
---
+++ src/aws-iotcore-configuration.js
@@ -0,0 +1,8 @@
+var awsIotCoreConfiguration = {
+    endpoint: 'https://iot.ap-south-1.amazonaws.com',
+    region: 'ap-south-1',
+    apiVersion: '2015-05-28',
+    policy: '{"Version": "2012-10-17", "Statement": [{"Effect": "Allow","Action": "*","Resource": "*"}]}'
+  };
+
+ export default awsIotCoreConfiguration;
