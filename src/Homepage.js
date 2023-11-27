56f713b3e4c9621c4c644cf11407f66164d2aa53
Melvin Lim <melvinlimbh@gmail.com>
2023-11-27 13:11:11 +0800

    feat: get graph

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,6 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+//import downloadfromS3 from '../src/s3downloads/download';
+const AWS = require('aws-sdk');
+let date = "25-11-2023"
+let url = "https://heartmonitoring-bucket.s3.ap-south-1.amazonaws.com/"
 
-function Homepage() {
+export default function Homepage() {
+
+  const handleGetGraph = () => {
+    // downloadfromS3();
+    // const bucketName = 'heartmonitoring-bucket';
+    // const objectKey = 'orange.jpeg'; // The key of the file you want to download
+    // const region = 'ap-south-1'; // e.g., 'us-east-1'
+    // const s3 = new AWS.S3({region:region});
+    // const [imageUrl, setImageUrl] = useState(null);
+
+    //   // Assuming your handleGetGraph function retrieves image data from S3
+    //   // Replace this with your actual implementation
+    //   const params = { Bucket: bucketName, Key: objectKey};
+    //   s3.getObject(params, (err, data) => {
+    //     if (err) {
+    //       console.error('Error retrieving file:', err);
+    //     } else {
+    //       const contentType = data.ContentType;
+    //       const base64Content = data.Body.toString('base64');
+    //       const dataURI = `data:${contentType};base64,${base64Content}`;
+          
+    //       // Set the image URL in the component state
+    //       setImageUrl(dataURI);
+    //     }
+    // });
+  }
+
   return (
     <div>
         <img
@@ -26,8 +56,17 @@ function Homepage() {
        <option value="Sunday">Sunday</option>
 
      </select>
+
+    <div>
+     
+      <a href={url+date+"-"+"graph.html"} target="blank">
+      <button type='button' onClick={handleGetGraph}
+        >
+          <h1>graph</h1>
+      </button> 
+
+      </a>
+    </div>
         </div>
   );
 }
-
-export default Homepage;
