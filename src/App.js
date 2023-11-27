d0ea42fddbc475363a795f06a9abbe2bc906784f
Leecz888 <leecz888@hotmail.com>
2023-11-27 23:01:07 +0800

    added refresh button

--- src/App.js
+++ src/App.js
@@ -1,33 +1,45 @@
-import React from 'react';
-import { withAuthenticator } from 'aws-amplify-react';
-import './App.css';
-import DatePicker from "react-datepicker"
+import React from "react";
+import { withAuthenticator } from "aws-amplify-react";
+import "./App.css";
+import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { useState } from 'react';
+import { useState } from "react";
 
-import MQTTConnect from './MQTTConnect'
-import Homepage from './Homepage'
+import MQTTConnect from "./MQTTConnect";
+import Homepage from "./Homepage";
+
+import dotenv from "dotenv";
+dotenv.config({ path: "../.env" });
 
 function App(props) {
+  const [startDate, setStartDate] = useState(new Date());
+  const [refresh, setRefresh] = useState(false);
 
-  const [startDate, setStartDate] = useState(new Date())
+  const handleRefresh = () => {
+    setRefresh(!refresh);
+  };
 
   return (
-    <div className='App'>
-      <div className='container'>
-        <div className='flex-container'>
-          <Homepage date={startDate}/>
-          <DatePicker
-            selected={startDate}
-            onChange={(date) => setStartDate(date)}
-            on
-          />
-        </div>
+    <div className="App">
+      <div className="container">
+        <div className="flex-container">
+          <Homepage date={startDate} refresh={refresh}/>
+          <div className='date-container'>
+            <button onClick={handleRefresh}>Refresh</button>
+            <div className='date-picker'>
+              Date:
+              <DatePicker
+                selected={startDate}
+                onChange={(date) => setStartDate(date)}
+                maxDate={new Date()}
+              />
+            </div>
+          </div>
+        </div> 
         <MQTTConnect />
       </div>
     </div>
   );
-
 }
 
 export default withAuthenticator(App, true);
--- src/Homepage.js
+++ src/Homepage.js
@@ -1,19 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import Plot from 'react-plotly.js'
+import AWS from "aws-sdk";
 
-export default function Homepage({ date }) {
+export default function Homepage({ date, refresh }) {
+
+  AWS.config.update({
+    accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
+  });
+
+  const S3_BUCKET = process.env.REACT_APP_AWS_S3_BUCKET_NAME + "-bucket";
+  const URL_EXPIRATION_TIME = 3600; 
+  
+  const myBucket = new AWS.S3({
+    params: { Bucket: S3_BUCKET },
+    region: process.env.REACT_APP_AWS_REGION,
+  });
 
   const [ plot, setPlot ] = useState(0)
 
   useEffect(() => {
     console.log(`${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`);
     let currentDate = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
-    fetch(`https://heartmonitoring-bucket.s3.ap-south-1.amazonaws.com/${currentDate}.json`)
-      .then(response => response.json())
-      .then(data => setPlot(data))
-      .catch(err => console.error("Error fetching json file:", err))
-  }, [date])
-
+    myBucket.getSignedUrl(
+      "getObject",
+      {
+        Key: `${currentDate}.json`,
+        Expires: URL_EXPIRATION_TIME,
+      },
+      (err, url) => {
+        fetch(url)
+          .then((response) => response.json())
+          .then((data) => setPlot(data))
+          .catch((err) => {
+            console.error("Error fetching json file:", err)
+            setPlot("")
+          });
+      }
+    );
+  }, [date, refresh])
 
   return (
     <div>
