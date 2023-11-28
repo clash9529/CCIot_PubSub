4ab5ff3c89ed419d0484d20810ec763debf5b3b2
Leecz888 <leecz888@hotmail.com>
2023-11-28 16:14:37 +0800

    integrated jiahui-branch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import { withAuthenticator } from 'aws-amplify-react';
-import './App.css';
-import DatePicker from "react-datepicker"
+import React from "react";
+import { withAuthenticator } from "aws-amplify-react";
+import "./App.css";
+import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { useState } from 'react';
+import { useState } from "react";
 
-import MQTTConnect from './MQTTConnect'
-import Homepage from './Homepage'
+import MQTTConnect from "./MQTTConnect";
+import Homepage from "./Homepage";
+
+import dotenv from "dotenv";
+dotenv.config({ path: "../.env" });
 
 function App(props) {
-
-  const [startDate, setStartDate] = useState(new Date())
-  const [refresh, setRefresh] = useState(false)
+  const [startDate, setStartDate] = useState(new Date());
+  const [refresh, setRefresh] = useState(false);
 
   const handleRefresh = () => {
-    setRefresh(!refresh)
-  }
+    setRefresh(!refresh);
+  };
 
   return (
-    <div className='App'>
-      <div className='container'>
-        <div className='flex-container'>
+    <div className="App">
+      <div className="container">
+        <div className="flex-container">
           <Homepage date={startDate} refresh={refresh}/>
           <div className='date-container'>
             <button onClick={handleRefresh}>Refresh</button>
@@ -29,15 +31,15 @@ function App(props) {
               <DatePicker
                 selected={startDate}
                 onChange={(date) => setStartDate(date)}
+                maxDate={new Date()}
               />
             </div>
           </div>
-        </div>
+        </div> 
         <MQTTConnect />
       </div>
     </div>
   );
-
 }
 
 export default withAuthenticator(App, true);
