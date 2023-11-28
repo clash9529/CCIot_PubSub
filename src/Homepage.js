af778d4484f24b83b8e965bccd82e8037dce633b
clash9529 <clash9529@gmail.com>
2023-11-28 14:06:01 +0800

    done

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,29 +1,127 @@
-import React, { useState, useEffect } from 'react';
-import Plot from 'react-plotly.js'
+import React, { useState, useEffect } from "react";
+import Plot from "react-plotly.js";
 
-export default function Homepage({ date, refresh }) {
+import AWS from "aws-sdk";
 
-  const [ plot, setPlot ] = useState(0)
+AWS.config.update({
+  accessKeyId: "AKIAY6F47JITDY6YNKJ7",
+  secretAccessKey: "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",
+});
+
+const S3_BUCKET = "heartmonitoring-bucket";
+const REGION = "ap-south-1";
+const URL_EXPIRATION_TIME = 3600; // in seconds
+
+const myBucket = new AWS.S3({
+  params: { Bucket: S3_BUCKET },
+  region: REGION,
+});
+
+function getPreviousDay(date = new Date()) {
+  const previous = new Date(date.getTime());
+  previous.setDate(date.getDate() - 1);
+
+  return previous;
+}
+
+function join(date, options, separator) {
+  function format(option) {
+    let formatter = new Intl.DateTimeFormat("en", option);
+    return formatter.format(date);
+  }
+  return options.map(format).join(separator);
+}
+
+export default function Homepage() {
+  const [plot, setPlot] = useState(0);
+  const [urll, setUrll] = useState("");
+  var listofDays = [];
+
+  var options = [{ day: "numeric" }, { month: "numeric" }, { year: "numeric" }];
+  var joined = join(new Date(), options, "-");
+
+  var tdy = new Date();
+
+  
+  listofDays.push(joined);
+  for (var i = 0; i < 6; i++) {
+    joined = join(getPreviousDay(tdy), options, "-");
+    tdy = getPreviousDay(tdy);
+
+    listofDays.push(joined);
+  }
+  console.log(listofDays);
 
   useEffect(() => {
-    console.log(`${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`);
-    let currentDate = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
-    fetch(`https://heartmonitoring-bucket.s3.ap-south-1.amazonaws.com/${currentDate}.json`)
-      .then(response => response.json())
-      .then(data => setPlot(data))
-      .catch(err => console.error("Error fetching json file:", err))
-  }, [date, refresh])
+    myBucket.getSignedUrl(
+      "getObject",
+      {
+        Key: joined+".json",
+        Expires: URL_EXPIRATION_TIME,
+      },
+      (err, url) => {
+        console.log(url);
+        console.log(err);
+        setUrll(url);
+        fetch(url)
+          .then((response) => response.json())
+          .then((data) => setPlot(data))
+          .catch((err) => console.error("Error fetching json file:", err));
+        return url; // API Response Here
+      }
+    );
+  }, []);
 
+  function handleChange(event) {
+    console.log(event.target.value);
+    myBucket.getSignedUrl(
+      "getObject",
+      {
+        Key: event.target.value+".json",
+        Expires: URL_EXPIRATION_TIME,
+      },
+      (err, url) => {
+        console.log(url);
+        console.log(err);
+        fetch(url)
+          .then((response) => response.json())
+          .then((data) => setPlot(data))
+          .catch((err) => {
+            console.error("Error fetching json file:", err)
+            setPlot("");
+          });
+        return url; // API Response Here
+      }
+    );
+  }
 
   return (
     <div>
       <img
-        src={'/VitalWatcher.png'}
+        src={"/VitalWatcher.png"}
         alt="Vital Watcher"
-        style={{ width: '80px', height: '80px', position: 'absolute', top: 0, left: 0 }}
+        style={{
+          width: "80px",
+          height: "80px",
+          position: "absolute",
+          top: 0,
+          left: 0,
+        }}
       />
-      <Plot data={plot.data} layout={plot.layout}/>
+      <Plot
+        // style={{ width: "300px", height: "200px" }}
+        data={plot.data}
+        layout={plot.layout}
+      />
+      <select onChange={handleChange}>
+        {listofDays.map((option) => {
+          return (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          );
+        })}
+      </select>
     </div>
   );
 }
-
